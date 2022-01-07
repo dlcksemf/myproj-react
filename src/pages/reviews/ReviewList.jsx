3994510cae9a338dfbe14a7c2ddc97efda586a45
Chandeul Lee <22chandl@gmail.com>
2022-01-07 14:06:14 +0900

    axios를 활용하여 review list api 호출

--- src/pages/reviews/ReviewList.jsx
+++ src/pages/reviews/ReviewList.jsx
@@ -1,7 +1,32 @@
+import Axios from 'axios';
+import { useEffect } from 'react';
+
 function ReviewList() {
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  const refetch = () => {
+    const url = 'http://127.0.0.1:8000/shop/api/reviews/';
+
+    // Promise 객체
+    // 상태코드 400 이상이면 오류 처리 이하면 정상 처리 => axios 자동 처리
+    Axios.get(url)
+      .then((response) => {
+        console.group('normal response');
+        console.log(response);
+        console.groupEnd();
+      })
+      .catch((error) => {
+        console.group('error response');
+        console.log(error);
+        console.groupEnd();
+      });
+  };
+
   return (
     <div>
-      <h2>Review List</h2>
+      <h2>hello</h2>
     </div>
   );
 }
