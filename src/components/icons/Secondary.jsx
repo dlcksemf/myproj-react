35019938dcfef0799d67e02f1fdbe53e52ce3b16
Chandeul Lee <22chandl@gmail.com>
2022-01-07 16:47:31 +0900

    class >> className으로 변경

--- src/components/icons/Secondary.jsx
+++ src/components/icons/Secondary.jsx
@@ -5,7 +5,7 @@ function Secondary() {
       focusable="false"
       data-prefix="fas"
       data-icon="circle"
-      class="w-4 h-4 fill-current"
+      className="w-4 h-4 fill-current"
       role="img"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 512 512"
