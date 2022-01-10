13ddf25983e202374e52ba5ed82f9dedc1846e14
Chrissy Lee <22chandl@gmail.com>
2022-01-10 21:03:36 +0900

    BlogDetail > img 태그 추가

--- src/components/blog/BlogDetail.jsx
+++ src/components/blog/BlogDetail.jsx
@@ -2,8 +2,10 @@ function BlogDetail({ post, loading }) {
   return (
     <div>
       {loading && <h2>loading...</h2>}
-      {!loading && <h2 className="text-lg m-2">{post.title}</h2>}
+      <h2 className="text-lg m-2">{post.title}</h2>
       {post.content}
+
+      <img src={"https://placeimg.com/640/480/tech"} />
     </div>
   );
 }
