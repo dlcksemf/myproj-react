033edbb7fd498d56226befd2220276f9de0f4a96
Chandeul Lee <22chandl@gmail.com>
2022-01-10 17:40:40 +0900

    BlogForm에 새로운 포스트 생성 기능 구현

--- src/pages/blog/PageBlogForm.jsx
+++ src/pages/blog/PageBlogForm.jsx
@@ -1,7 +1,38 @@
+import BlogForm from 'components/blog/BlogForm';
+import useFieldValues from 'components/hooks/useFieldValues';
+import Axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 function PageBlogForm() {
+  const navigate = useNavigate();
+
+  const [fieldValues, handleFieldChange, emptyFieldValues, setFieldValues] =
+    useFieldValues({ content: '', title: '' });
+
+  const submitReview = (e) => {
+    e.preventDefault();
+
+    const url = 'http://localhost:8000/blog/api/posts/';
+
+    Axios.post(url, fieldValues)
+      .then(() => {
+        navigate('/blog/');
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        emptyFieldValues();
+      });
+  };
+
   return (
     <div>
-      <h2>PageBlogForm</h2>
+      <BlogForm
+        submitReview={submitReview}
+        handleChange={handleFieldChange}
+        fieldValues={fieldValues}
+      />
     </div>
   );
 }
