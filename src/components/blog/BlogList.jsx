a5c3872b81b64faff6bc38821c69fc4e48898eb8
Chandeul Lee <22chandl@gmail.com>
2022-01-10 17:05:47 +0900

    PostList 컴포넌트 및 PageBlogList 레이아웃 구현

---
+++ src/components/blog/BlogList.jsx
@@ -0,0 +1,82 @@
+function BlogList({ post }) {
+  const { title, content } = post;
+
+  // TODO : image / button 기능 구현 / style로 content 말줄임
+  return (
+    <div>
+      <section className="pt-20 lg:pt-[120px] pb-10 lg:pb-20 bg-[#F3F4F6]">
+        <div className="container">
+          <div className="flex flex-wrap -mx-4">
+            <div className="w-full px-4">
+              <div className="bg-white rounded-lg overflow-hidden mb-10">
+                {/* <img
+                  src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-02.jpg"
+                  alt="image"
+                  className="w-full"
+                /> */}
+                <div className="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
+                  <h3>
+                    {/* navigate로 detail 페이지로 */}
+                    <div
+                      className="
+                        font-semibold
+                        text-dark text-xl
+                        sm:text-[22px]
+                        md:text-xl
+                        lg:text-[22px]
+                        xl:text-xl
+                        2xl:text-[22px]
+                        mb-4
+                        block
+                        hover:text-primary
+                        "
+                    >
+                      {title}
+                    </div>
+                  </h3>
+                  <p className="text-base text-body-color leading-relaxed mb-7">
+                    {content}
+                  </p>
+
+                  {/* 수정 / 삭제 버튼 */}
+                  <div
+                    className="
+                     inline-block
+                     py-2
+                     px-7
+                     border border-[#E5E7EB]
+                     rounded-full
+                     text-base text-body-color
+                     font-medium
+                     hover:border-primary hover:bg-primary hover:text-white
+                     transition
+                     "
+                  >
+                    EDIT
+                  </div>
+                  <div
+                    className="
+                     inline-block
+                     py-2
+                     px-7
+                     border border-[#E5E7EB]
+                     rounded-full
+                     text-base text-body-color
+                     font-medium
+                     hover:border-primary hover:bg-primary hover:text-white
+                     transition
+                     "
+                  >
+                    DELETE
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default BlogList;
--- src/pages/blog/PageBlogList.jsx
+++ src/pages/blog/PageBlogList.jsx
@@ -1,7 +1,41 @@
+import BlogList from 'components/blog/BlogList';
+import Axios from 'axios';
+import { useEffect, useState } from 'react';
+import DebugStates from 'components/DebugStates';
+
 function PageBlog() {
+  const [postList, setPostList] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  const refetch = () => {
+    setError(null);
+    setLoading(true);
+    const url = 'http://localhost:8000/blog/api/posts/';
+
+    Axios.get(url)
+      .then(({ data }) => {
+        setPostList(data);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <div>
-      <h2 className="text-red-400 text-lg">PageBlog</h2>
+      <DebugStates postList={postList} />
+
+      {postList.map((post) => {
+        return <BlogList post={post} />;
+      })}
     </div>
   );
 }
