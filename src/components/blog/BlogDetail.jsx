043e611f966ccae974788ee7267aacdbce00d83e
Chandeul Lee <22chandl@gmail.com>
2022-01-13 10:04:20 +0900

    blog detail 페이지 스타일링 개선

--- src/components/blog/BlogDetail.jsx
+++ src/components/blog/BlogDetail.jsx
@@ -76,10 +76,29 @@ function BlogDetail({ postId }) {
         />
       )}
 
-      <div className="flex justify-center items-center rounded grid md:grid-cols-2 grid-cols-1 shadow-2xl bg-slate-800 xl:w-4/5 md:w-full md:h-4/5 h-full w-96 text-center text-sm rounded-sm">
+      <div className="flex justify-center items-center rounded grid grid-cols-1 shadow-2xl bg-slate-800 w-4/5 h-full w-96 text-center text-sm rounded-sm">
         <div className="px-3">
           <img src="https://placeimg.com/640/480/tech" alt="tech photos" />
+        </div>
 
+        {post && (
+          <div className="bg-my-color text-white w-full h-full flex justify-center items-center px-3 py-4 text-center">
+            <div className="h-full px-8 text-gray-100 lg:text-lg">
+              <div className="font-sans md:mb-3 mb-2">
+                <h2 className="uppercase font-medium">{post.title}</h2>
+              </div>
+              <p className="lg:text-md text-sm">
+                {post.content.split(/[\r\n]+/).map((line, index) => (
+                  <p className="my-3" key={index}>
+                    {line}
+                  </p>
+                ))}
+              </p>
+            </div>
+          </div>
+        )}
+
+        <div>
           <div
             onClick={() => {
               navigate(`/blog/${post.id}/edit/`);
@@ -123,25 +142,14 @@ function BlogDetail({ postId }) {
           >
             DELETE
           </div>
-
-          <div
-            onClick={() => navigate('/blog/')}
-            className="my-5 cursor-pointer transition-all duration-150 hover:bg-green-500 bg-green-400 md:px-12  sm:px-10 sm:py-3 px-8 py-2 text-center rounded-full md:text-xl text-md text-my-color font-medium"
-          >
-            Back to List
-          </div>
         </div>
 
-        {post && (
-          <div className="bg-my-color text-white w-full h-full flex justify-center items-center px-3 py-4 text-center">
-            <div className="h-full px-8 text-gray-100 lg:text-lg">
-              <div className="font-sans md:mb-3 mb-2">
-                <h2 className="uppercase font-medium">{post.title}</h2>
-              </div>
-              <p className="lg:text-md text-sm">{post.content}</p>
-            </div>
-          </div>
-        )}
+        <div
+          onClick={() => navigate('/blog/')}
+          className="m-5 cursor-pointer transition-all duration-150 hover:bg-green-500 bg-green-400 md:px-12  sm:px-10 sm:py-3 px-8 py-2 text-center rounded-full md:text-xl text-md text-my-color font-medium"
+        >
+          Back to List
+        </div>
       </div>
     </>
   );
