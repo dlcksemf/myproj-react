f7dfce878a165443e80b870ae14c7c9b4f94411d
Chandeul Lee <22chandl@gmail.com>
2022-01-14 10:32:23 +0900

    EmojisList 페이지 구현

--- src/components/emojis/EmojisList.jsx
+++ src/components/emojis/EmojisList.jsx
@@ -1,5 +1,43 @@
+import { useEffect } from 'react';
+import DebugStates from 'components/DebugStates.jsx';
+import { useApiAxios } from 'api/base.js';
+import { useNavigate } from 'react-router-dom';
+import H2 from 'components/H2';
+import Button from 'components/Button';
+
 function EmojisList() {
-  return <div>Emojis List</div>;
+  const navigate = useNavigate();
+  const [{ data, loading, error }, refetch] = useApiAxios('emojis/api/emojis', {
+    manual: true,
+  });
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          navigate('/emojis/new/');
+        }}
+      >
+        New Emoji
+      </Button>
+      {data?.map(({ name, apple_version, id: emojiId }) => (
+        <div
+          onClick={() => {
+            navigate(`/emojis/${emojiId}/`);
+          }}
+          className="m-2 p-2 border border-red-400 cursor-pointer"
+        >
+          <H2>{name}</H2>
+          <img src={apple_version} alt={name} />
+        </div>
+      ))}
+      <DebugStates data={data} />
+    </div>
+  );
 }
 
 export default EmojisList;
