0de7f4a823dce548c3d9cfcb2507fefe1f9e02b9
Chandeul Lee <22chandl@gmail.com>
2022-01-18 10:21:33 +0900

    LoginForm 컴포넌트에서 JWT 로그인을 구현

--- src/pages/accounts/PageLogin.jsx
+++ src/pages/accounts/PageLogin.jsx
@@ -1,69 +1,9 @@
-import { useApiAxios } from 'api/base';
-import DebugStates from 'components/DebugStates';
-import useFieldValues from 'components/hooks/useFieldValues';
-
-// https://tailwindcomponents.com/component/login-form-14
-
-const INIT_FIELD_VALUES = { username: '', password: '' };
+import LoginForm from 'components/accounts/LoginForm';
 
 function PageLogin() {
-  const [{ loading, error }, requestToken] = useApiAxios(
-    {
-      url: 'http://127.0.0.1:8000/accounts/api/token/',
-      method: 'POST',
-    },
-    {
-      manual: true,
-    },
-  );
-  const { handleFieldChange, fieldValues } = useFieldValues(INIT_FIELD_VALUES);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    requestToken({ data: fieldValues }).then((response) => {
-      console.log(response.data);
-    });
-  };
-
   return (
     <div>
-      <h2>Login</h2>
-
-      <div className="flex items-center justify-center h-screen">
-        <div className="bg-gray-800 flex flex-col w-80 border border-gray-900 rounded-lg px-8 py-10">
-          <div className="text-white mt-10">
-            <h1 className="font-bold text-4xl">Welcome</h1>
-            <p className="font-semibold">Let's Login!</p>
-          </div>
-
-          <form
-            onSubmit={handleSubmit}
-            className="flex flex-col space-y-8 mt-10"
-          >
-            <input
-              type="text"
-              name="username"
-              onChange={handleFieldChange}
-              value={fieldValues.username}
-              placeholder="Username"
-              className="border rounded-lg py-3 px-3 bg-gray-700 border-gray-700 placeholder-gray-500 text-white"
-            />
-            <input
-              type="password"
-              name="password"
-              onChange={handleFieldChange}
-              value={fieldValues.password}
-              placeholder="******************"
-              className="border rounded-lg py-3 px-3 bg-gray-700 border-gray-700 placeholder-gray-500 text-white"
-            />
-            <button className="border border-indigo-100 bg-indigo-100 text-white rounded-lg py-3 font-semibold">
-              Submit
-            </button>
-          </form>
-        </div>
-      </div>
-
-      <DebugStates fieldValues={fieldValues} />
+      <LoginForm />
     </div>
   );
 }
